3fd2ba87f47ce477ceffd1b6e72aee1643467cb0
Christian <christianlance85@gmail.com>
2021-06-15 15:59:48 -0500

    file restructure

---
+++ codecomp/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import {hot} from 'react-hot-loader/root'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This will be CodeComp!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default hot(App);
